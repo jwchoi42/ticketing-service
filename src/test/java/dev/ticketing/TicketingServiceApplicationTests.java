8cbbde5991e4ebd5fb1f9eecaf7330278920e785
jwchoi <jwchoi42@naver.com>
2026-01-07 21:11:05 +0900

    init: initialize project

---
+++ src/test/java/dev/ticketing/TicketingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
